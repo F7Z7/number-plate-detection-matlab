1ac28dea861404df374da7ecfdfccac15014c455
farza <farzanrichu610@gmail.com>
2025-10-01 01:22:16 +0530

    set up the edge detection and smoothen the image using otsu technique

--- main.m
+++ main.m
@@ -1,6 +1,16 @@
+%load image and convert to grayscale
 img=imread("car-17.png");
 gray = rgb2gray(img);
+
+%smooth and rmeove noise
+gray=medfilt2(gray,[3 3]);
 level = graythresh(gray);     % Otsu’s method (automatic threshold)
-bw = imbinarize(gray,level);
+
+%reformat the image
+bw = imbinarize(gray,level); %binarize to 0 and 1 
+bw=imcomplement(bw); %complement 1 to 0 and vice versa
+
+%edge detections
+bw=edge(bw,"canny");
 imshow(bw);
 
